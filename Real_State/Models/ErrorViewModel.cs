4223fbd78ef48dad43aea35001d7e3f45135bf65
Brayan Hernández <brayannhc07@gmail.com>
2022-03-23 13:51:48 -0600

    init commit

---
+++ Real_State/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Real_State.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
